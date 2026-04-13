dc0545694a2786352d5fc808f1086c1ed0218d82
osso2k <ossoble817@gmail.com>
2026-04-13 05:49:38 +0300

    Starting auth implementaion.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,19 +2,19 @@ import profile from '../assets/profile.png'
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 w-full h-[12%] mt-2  px-6">
+    <div className="flex justify-between sm:grid sm:grid-cols-2 md:grid-cols-3 w-full h-[12%] mt-2  px-6">
         <div className='mx-auto my-auto'>
-            <h1 className="font-minecraft text-3xl pl-20 pt-6  " >Forkymarket</h1>
+            <h1 className="font-minecraft text-3xl sm:pl-14 md:pl-20 pt-6" >Forkymarket</h1>
         </div>
-        <div className="flex text-xl mx-auto my-auto">
+        <div className="hidden md:flex text-xl mx-auto my-auto">
             <ul className="flex border-b border-zinc-800 font-minecraft text-xl list-none gap-6 pt-8 pb-2">
-            <li className="cursor-pointer hover:text-zinc-700 transition-all ">markets</li>
-            <li className="cursor-pointer hover:text-zinc-700 transition-all ">favs</li>
-            <li className="cursor-pointer hover:text-zinc-700 transition-all ">analysis</li>
+                <li className="cursor-pointer hover:text-zinc-700 transition-all ">markets</li>
+                <li className="cursor-pointer hover:text-zinc-700 transition-all ">favs</li>
+                <li className="cursor-pointer hover:text-zinc-700 transition-all ">analysis</li>
             </ul>
         </div>
         <div className='pt-4 mx-auto my-auto'>
-            <img className='cursor-pointer h-12 w-12' src={profile} alt="pic" />
+            <img className='cursor-pointer h-12 w-12 hover:opacity-55 transition-all hover:scale-[99%]' src={profile} alt="pic" />
         </div>
 
     </div>
